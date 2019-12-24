c346e89c29f881e88c9605dbe1f4cca7a0382275
beaulieu527 <beaulieudev@gmail.com>
2019-12-24 17:54:53 -0500

    Added Toastify For notifications

--- src/App.js
+++ src/App.js
@@ -10,9 +10,25 @@ import Training from './components/training/Training'
 import Deals from './components/deals/Deals'
 import Navbar from  './components/navbar/Navbar'
 import Footer from  './components/navbar/Footer'
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   return (
     <div className="App">
+       <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnVisibilityChange
+                draggable
+                pauseOnHover
+              />
+
       <Router>
             <div>
               <Switch>
