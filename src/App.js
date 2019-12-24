756f93705b387040382b92272ec634979c58e53f
beaulieu527 <beaulieudev@gmail.com>
2019-12-24 17:52:04 -0500

    added react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from './components/home/Home'
+import About from './components/about/About'
+import Team from './components/about/Team'
+import Contact from './components/contact/Contact'
+import FitClasses from './components/fitness-class/FitClasses'
+import Training from './components/training/Training'
+import Deals from './components/deals/Deals'
+import Navbar from  './components/navbar/Navbar'
+import Footer from  './components/navbar/Footer'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+            <div>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+                <Route path="/team" component={Team} />
+                <Route path="/contact" component={Contact} />
+                <Route path="/fitness-classes" component={FitClasses} />
+                <Route path="/training" component={Training} />
+                <Route path="/deals" component={Deals} />
+              </Switch>
+            </div>
+          </Router>
     </div>
   );
 }
