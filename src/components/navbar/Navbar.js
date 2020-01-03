6a9ab96b292715bd918b7cd64382178b889406f2
kyoung90 <kenneth.young90@gmail.com>
2020-01-02 21:29:45 -0400

    Made navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,15 +1,79 @@
-import React, { Component } from 'react';
-import { Link as RouterLink } from 'react-router-dom';
-
-
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-    
-    return (
-        <div>
-        
-        </div>
-    );
-}
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        New England Functional Fitness
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink
+              exact
+              className="nav-link"
+              activeClassName="navlink-active"
+              to="/"
+            >
+              About
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              exact
+              className="nav-link"
+              activeClassName="navlink-active"
+              to="/"
+            >
+              Contact
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              exact
+              className="nav-link"
+              activeClassName="navlink-active"
+              to="/"
+            >
+              Classes
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              exact
+              className="nav-link"
+              activeClassName="navlink-active"
+              to="/"
+            >
+              Training
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              exact
+              className="nav-link"
+              activeClassName="navlink-active"
+              to="/"
+            >
+              Deals
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
