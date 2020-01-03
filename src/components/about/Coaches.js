07e868919b21eb70ea5e8f3526124c08c120d648
kyoung90 <kenneth.young90@gmail.com>
2020-01-03 18:57:24 -0400

    Coaches divider header in progress

--- src/components/about/Coaches.js
+++ src/components/about/Coaches.js
@@ -6,10 +6,20 @@ const Coaches = () => {
     <div>
       <div className="banner">
         <div className="d-flex justify-content-center align-items-center blue-overlay">
-          <h1 className="section-header">
-            Meet The Coaches
-          </h1>
+          <h1 className="section-header">Meet The Coaches</h1>
+
+          <nav aria-label="breadcrumb">
+          <ol class="breadcrumb divider-header">
+            <li class="breadcrumb-item">
+                Your Team
+            </li>
+            <li class="breadcrumb-item active" aria-current="page">
+              Your Coaches
+            </li>
+          </ol>
+        </nav>
         </div>
+        
       </div>
     </div>
   );
