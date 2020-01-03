4b3aa5feb28f90a38f73aca85a40fa180d925769
kyoung90 <kenneth.young90@gmail.com>
2020-01-03 18:35:04 -0400

    Added coaches banner

--- src/components/about/Coaches.js
+++ src/components/about/Coaches.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./Coaches.css";
 
 const Coaches = () => {
-    return (
-        <div>
-            <h1>Noooooooooooooo</h1>
+  return (
+    <div>
+      <div className="banner">
+        <div className="d-flex justify-content-center align-items-center blue-overlay">
+          <h1 className="section-header">
+            Meet The Coaches
+          </h1>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Coaches;
